d7b3bd88cee2e927c1781c96e54d38867b993788
M7mdKady14 <mohamadahmadme@gmail.com>
2026-02-28 02:29:31 +0200

    add the generic repository implementation

---
+++ MediCare.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using MediCare.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCare.Data.Repositories
+{
+    public class GenericRepository<T, TKey> : IGenericRepository <T, TKey>
+        where T : class
+    {
+        protected AppDbContext _context;
+        public GenericRepository(AppDbContext context) { 
+            _context = context;
+        }
+        public async Task<T> Add(T entity)
+        {
+            var entry = await _context.Set<T>().AddAsync(entity);
+            return entry.Entity;
+        }
+
+        public async Task Delete(TKey id)
+        {
+            var entry = await _context.Set<T>().FindAsync(id);
+            if (entry == null)
+            {
+                throw new KeyNotFoundException($"Entity with type: {typeof(T)} and id: {id} not found.");
+            }
+            _context.Set<T>().Remove(entry);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            var entries = await _context.Set<T>().ToListAsync();
+            return entries;
+        }
+
+        public async Task<T?> GetById(TKey id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
