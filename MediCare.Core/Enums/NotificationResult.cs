ee116862cdd12beae82edecc992cfaeb51d38f25
M7mdKady14 <mohamadahmadme@gmail.com>
2026-05-13 13:45:29 +0300

    forgot this enum

---
+++ MediCare.Core/Enums/NotificationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCare.Core.Enums
+{
+    public class NotificationResult
+    {
+        public bool Success { get; set; }
+        public string? ErrorMessage { get; set; }
+        public DateTime SentAt { get; set; }
+    }
+
+}
